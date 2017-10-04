0a626ce19057f88118e871267ec18a7f3d721534
Mark Philip Dawal <radical.mark@gmail.com>
2017-10-05 00:38:01 +0400

    successfully added commitment textarea with autoexpand feature

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 $( document ).ready(function() {
+    $("#menu--sidebar").sideNav();
+});
 
+
+$( document ).on('focus.autoExpand', 'textarea.autoExpand', function(){
+    var savedValue = this.value;
+    this.value = '';
+    this.baseScrollHeight = this.scrollHeight;
+    this.value = savedValue;
+}).on('input.autoExpand', 'textarea.autoExpand', function(){
+    var minRows = this.getAttribute('data-min-rows')|0, rows;
+    this.rows = minRows;
+    console.log(this.scrollHeight, "scroll height");
+    console.log(this.baseScrollHeight, "base scroll height");
+    rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 22);
+    this.rows = minRows + rows;
 });
