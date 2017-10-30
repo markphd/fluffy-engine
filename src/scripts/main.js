ec93e80b981bbc86c823a865d31ef27c45ac83c7
Mark Philip Dawal <radical.mark@gmail.com>
2017-10-31 02:52:57 +0400

    changes made for edit-commitment page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,17 @@ $( document ).ready(function() {
     });
 
     $('select').material_select();
+
+    var picker = new Pikaday({ field: $('#datepicker')[0], showDaysInNextAndPreviousMonths: true, enableSelectionDaysInNextAndPreviousMonths: true });
+
+    $.mask.definitions['H'] = "[0-1]";
+    $.mask.definitions['h'] = "[0-9]";
+    $.mask.definitions['M'] = "[0-5]";
+    $.mask.definitions['m'] = "[0-9]";
+    $.mask.definitions['P'] = "[AaPp]";
+    $.mask.definitions['p'] = "[Mm]";
+
+    $(".edit--time").mask("Hh:Mm Pp");
 });
 
 $( document ).on('focus.autoExpand', 'textarea.autoExpand', function(){
