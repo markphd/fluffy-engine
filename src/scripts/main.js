3fbf36a3a9140833a65aebecd75842979ffabdaa
Mark <radical.mark@gmail.com>
2017-10-31 13:29:30 +0400

    trim unnecessary scripts and implemented timepicker

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,14 +12,7 @@ $( document ).ready(function() {
 
     var picker = new Pikaday({ field: $('#datepicker')[0], showDaysInNextAndPreviousMonths: true, enableSelectionDaysInNextAndPreviousMonths: true });
 
-    $.mask.definitions['H'] = "[0-1]";
-    $.mask.definitions['h'] = "[0-9]";
-    $.mask.definitions['M'] = "[0-5]";
-    $.mask.definitions['m'] = "[0-9]";
-    $.mask.definitions['P'] = "[AaPp]";
-    $.mask.definitions['p'] = "[Mm]";
-
-    $(".edit--time").mask("Hh:Mm Pp");
+    $('#timepicker').timepicker({ 'scrollDefault': 'now' });
 });
 
 $( document ).on('focus.autoExpand', 'textarea.autoExpand', function(){
